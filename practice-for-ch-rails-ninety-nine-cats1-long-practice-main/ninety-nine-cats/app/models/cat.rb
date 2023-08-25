116f58e1f2f2bdb49b59af74940b9f56b1c25ba3
Mitchell Chan <mitchellkchan@gmail.com>
2023-08-25 14:36:17 -0700

    set up Cat model

---
+++ practice-for-ch-rails-ninety-nine-cats1-long-practice-main/ninety-nine-cats/app/models/cat.rb
@@ -0,0 +1,36 @@
+# == Schema Information
+#
+# Table name: cats
+#
+#  id          :bigint           not null, primary key
+#  birth_date  :date             not null
+#  color       :string           not null
+#  name        :string           not null
+#  sex         :string(1)        not null
+#  description :text
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+require "action_view"
+
+class Cat < ApplicationRecord
+    include ActionView::Helpers::DateHelper 
+    CAT_COLORS = ["white", "black", "grey", "orange"]
+
+    validates :birth_date, :color, :name, :sex, presence: true
+    validates :color, inclusion: {in: CAT_COLORS}
+    validates :sex, inclusion: {in: ["M", "F"]}
+    validate :birth_date_cannot_be_future
+
+    def birth_date_cannot_be_future
+        if birth_date > Date.today
+            errors.add(:birth_date, "can't be in the future")
+        end
+    end
+
+    def age
+        time_ago_in_words(birth_date.to_time)
+    end
+
+end
